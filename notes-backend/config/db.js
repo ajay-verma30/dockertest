47e58f225e4b522de6eb098bf9a362f39f1ecd81
Ajay Verma <aryanav30@gmail.com>
2026-04-05 17:02:57 +0530

    Added Docker compose

--- notes-backend/config/db.js
+++ notes-backend/config/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const uri = `mongodb+srv://aryan2130:P3p3aD2en3nQwWob@cluster0.ri4ol.mongodb.net/notes?retryWrites=true&w=majority`;
-        
+        // const uri = `mongodb+srv://aryan2130:P3p3aD2en3nQwWob@cluster0.ri4ol.mongodb.net/notes?retryWrites=true&w=majority`;
+
+        const uri = process.env.MONGO_URI;
         await mongoose.connect(uri);
         console.log("MongoDB Connected via Mongoose...");
     } catch (err) {
